20092a1c438f39e97ef0b233148ff9574a00e85c
Тимофей Канаш <gr424_katki@lab-305-4.tomtit-tomsk.ru>
2024-11-12 13:46:25 +0700

    add change password

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index, other_page, BBLoginView, BBLogoutView, profile, ChangeUserInfoView
+from .views import index, other_page, BBLoginView, BBLogoutView, profile, ChangeUserInfoView, BBPasswordChangeView
 
 app_name = 'main'
 
 urlpatterns = [
+   path('accounts/password/change/', BBPasswordChangeView.as_view(), name='password_change'),
    path('accounts/profile/change/', ChangeUserInfoView.as_view(), name='profile_change'),
    path('accounts/profile/', profile, name='profile'),
    path('accounts/logout/', BBLogoutView.as_view(), name='logout'),
