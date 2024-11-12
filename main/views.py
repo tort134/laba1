77b22230c9032b1a088348cb0be4e6093752b88a
Тимофей Канаш <gr424_katki@lab-305-4.tomtit-tomsk.ru>
2024-11-12 13:38:47 +0700

    add profile

--- main/views.py
+++ main/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from django.template import TemplateDoesNotExist
 from django.template.loader import get_template
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.views.generic import UpdateView
+
+from .forms import ChangeUserInfoForm
+from .models import AdvUser
+
 
 def index(request):
    return render(request, 'main/index.html')
@@ -21,3 +29,23 @@ class BBLoginView(LoginView):
 @login_required
 def profile(request):
    return render(request, 'main/profile.html')
+
+class BBLogoutView(LoginRequiredMixin, LogoutView):
+   template_name = 'main/logout.html'
+
+class ChangeUserInfoView(SuccessMessageMixin, LoginRequiredMixin,
+                        UpdateView):
+   model = AdvUser
+   template_name = 'main/change_user_info.html'
+   form_class = ChangeUserInfoForm
+   success_url = reverse_lazy('main:profile')
+   success_message = 'Личные данные пользователя изменены'
+
+   def dispatch(self, request, *args, **kwargs):
+       self.user_id = request.user.pk
+       return super().dispatch(request, *args, **kwargs)
+
+   def get_object(self, queryset=None):
+       if not queryset:
+           queryset = self.get_queryset()
+       return get_object_or_404(queryset, pk=self.user_id)
